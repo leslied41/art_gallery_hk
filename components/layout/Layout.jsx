861fd880e6ab39f3a7d6ed85869b19aa9e9cc702
leslied41 <leslied41@gmail.com>
2021-12-20 21:10:12 +1100

    conditional font-family based on locale

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,13 +1,17 @@
 import MainNavigation from "./MainNavigation";
 import Footer from "./Footer";
 import styles from "./Layout.module.css";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
+  const router = useRouter();
   return (
     <>
-      <MainNavigation />
-      <main className={styles.layout}>{children}</main>
-      <Footer />
+      <div className={router.locale}>
+        <MainNavigation />
+        <main className={styles.layout}>{children}</main>
+        <Footer />
+      </div>
     </>
   );
 }
