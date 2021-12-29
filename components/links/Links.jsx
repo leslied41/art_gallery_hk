f89e3a3b8ef330dd9607691d75dea571646230d2
leslied41 <leslied41@gmail.com>
2021-12-29 21:37:09 +1100

    fix LINKS TRANSLATE

--- components/links/Links.jsx
+++ components/links/Links.jsx
@@ -1,8 +1,9 @@
 import styles from "./Links.module.css";
 import Link from "next/link";
 import LanguageSwitch from "../languageSwitch/LanguageSwitch";
-
+import { useRouter } from "next/router";
 const Links = ({ lan }) => {
+  const router = useRouter();
   if (lan) {
     return (
       <>
@@ -10,27 +11,39 @@ const Links = ({ lan }) => {
           <ul className={styles.links}>
             <li>
               <Link href="/artists" exact>
-                <span className="h4">artist</span>
+                <span className="h4">
+                  {router.locale == "en" ? "Artist" : "藝術家"}
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/exhibitions" exact>
-                <span className="h4">exhibitions</span>
+                <span className="h4">
+                  {router.locale == "en" ? "Exhibitions" : "展出"}
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/news" exact>
-                <span className="h4"> news</span>
+                <span className="h4">
+                  {" "}
+                  {router.locale == "en" ? "News" : "新聞"}
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/study" exact>
-                <span className="h4"> publications</span>
+                <span className="h4">
+                  {" "}
+                  {router.locale == "en" ? "Study" : "書齋"}
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/about" exact>
-                <span className="h4">about</span>
+                <span className="h4">
+                  {router.locale == "en" ? "About" : "關於我們"}
+                </span>
               </Link>
             </li>
             <li>
