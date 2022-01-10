134e257a113e67b4ad40dc15999e8eb13c4cea8d
leslied41 <leslied41@gmail.com>
2022-01-10 21:03:00 +1100

    STUDY SVG MOVE BUG

--- components/s_layout/S_layout.jsx
+++ components/s_layout/S_layout.jsx
@@ -1,12 +1,16 @@
 import S_header from "./S_header";
 import Footer from "../layout/Footer";
 import styles from "./S_layout.module.css";
+import { useRouter } from "next/router";
 
 const S_layout = ({ children }) => {
+  const router = useRouter();
   return (
     <>
-      <S_header />
-      {children}
+      <div className={router.locale}>
+        <S_header />
+        {children}
+      </div>
     </>
   );
 };
