8e8db7883c5929b54adff28b3e35bf590e90d8ac
leslied41 <leslied41@gmail.com>
2022-03-25 18:30:32 +1100

    using css to implement pc or mobile  header

--- components/dropDownCard/ExListWorks.jsx
+++ components/dropDownCard/ExListWorks.jsx
@@ -7,13 +7,55 @@ import ReactPlayer from "react-player";
 import { useRef, useState, useEffect, useLayoutEffect, createRef } from "react";
 import styles from "./ExListWorks.module.css";
 import { PortableText } from "@portabletext/react";
-import { usePortableText } from "../usehooks/usePortableText";
+import { getImageDimensions } from "@sanity/asset-utils";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
   return builder.image(source);
 }
 
+const SampleImageComponent = ({ value }) => {
+  const { width, height } = getImageDimensions(value);
+
+  return (
+    <img
+      src={urlFor().image(value).fit("max").auto("format").url()}
+      alt={value.alt || " "}
+      loading="lazy"
+      style={
+        width / height >= 1
+          ? {
+              // Avoid jumping around with aspect-ratio CSS property
+              //aspectRatio: width / height,
+              objectFit: "contain",
+              // width: "100%",
+              width: "100%",
+              //width: "auto",
+            }
+          : {
+              objectFit: "contain",
+              height: "100%",
+            }
+      }
+    />
+  );
+};
+
+const serializers = {
+  marks: {
+    link: ({ children, value }) =>
+      value.blank ? (
+        <a href={value.href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      ) : (
+        <a href={value.href}>{children}</a>
+      ),
+  },
+  types: {
+    image: SampleImageComponent,
+  },
+};
 const ExListWorks = ({ data }) => {
   //const { showCard } = useContext(dropDownContext);
   const { works, exhibition_works } = data;
@@ -353,7 +395,14 @@ const ExListWorks = ({ data }) => {
                             : { display: "none" }
                         }
                       >
-                        {work_parameter && usePortableText(work_parameter)}
+                        {work_parameter && (
+                          <PortableText
+                            value={work_parameter}
+                            components={serializers}
+                            projectId="z3dq9mvc"
+                            dataset="production"
+                          />
+                        )}
                       </div>
                     )}
                   </div>
