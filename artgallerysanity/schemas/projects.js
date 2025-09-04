9161323e319d541b7c50287539637ab80999fe92
Victoria <victoria@Victorias-MacBook-Pro.local>
2025-09-03 20:17:16 -0400

    update sanity test

---
+++ artgallerysanity/schemas/projects.js
@@ -0,0 +1,182 @@
+// /schemas/project.ts
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
+  fieldsets: [
+    {
+      name: "translation",
+      title: "Translation",
+      options: {
+        collapsible: true, // Makes the whole fieldset collapsible
+        collapsed: true, // Defines if the fieldset should be collapsed by default or not
+      },
+    },
+  ],
+  fields: [
+    {
+      name: "name_exo",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "name_exo_cn",
+      title: "Name Chinese",
+      type: "string",
+      fieldset: "translation",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name_exo",
+        maxLength: 96,
+      },
+    },
+
+    {
+      name: "introduction",
+      title: "Introduction",
+      type: "blockContent",
+    },
+    {
+      name: "date",
+      title: "Date",
+      type: "string",
+    },
+    {
+      name: "time_for_reorder",
+      title: "Time for Reorder",
+      type: "datetime",
+      description: "the project with latest time will be on the top",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+    },
+    {
+      name: "layout",
+      title: "Image Layout",
+      type: "string",
+      options: {
+        list: [
+          { title: "Full Width", value: "vertical" },
+          { title: "Half Width", value: "horizontal" },
+        ],
+      },
+    },
+    {
+      name: "image_parameter",
+      title: "Image Parameter",
+      type: "blockContent",
+    },
+    {
+      name: "date_cn",
+      title: "Date Chinese",
+      type: "string",
+      fieldset: "translation",
+    },
+
+    {
+      name: "introduction_cn",
+      title: "Introduction Chinese",
+      type: "blockContent",
+      fieldset: "translation",
+    },
+    {
+      name: "project_works",
+      title: "Project Works",
+      type: "array",
+      of: [
+        {
+          name: "project_work",
+          title: "Work",
+          type: "object",
+          fields: [
+            { name: "work_image", title: "Image", type: "image" },
+            {
+              name: "video_url",
+              title: "video",
+              type: "url",
+            },
+            {
+              name: "layout",
+              title: "Layout",
+              type: "string",
+              initialValue: "vertical",
+              options: {
+                list: [
+                  { title: "Full Width", value: "vertical" },
+                  { title: "Image Left Half Width", value: "horizontal" },
+                  {
+                    title: "Image Right Half Width",
+                    value: "right_horizontal",
+                  },
+                ],
+              },
+            },
+            {
+              name: "name",
+              title: "Work Name",
+              type: "string",
+            },
+            {
+              name: "name_cn",
+              title: "Work Name Chinese",
+              type: "string",
+            },
+            {
+              name: "introduction",
+              title: "Introduction ",
+              type: "blockContent",
+            },
+            {
+              name: "introduction_cn",
+              title: "Introduction Chinese ",
+              type: "blockContent",
+            },
+            {
+              name: "work_parameter",
+              title: "Work parameter ",
+              type: "blockContent",
+            },
+            {
+              name: "buttons",
+              title: "Button Group ",
+              type: "array",
+              of: [
+                {
+                  name: "custom_button",
+                  title: "Button",
+                  type: "object",
+                  fields: [
+                    {
+                      name: "button_name",
+                      title: "Button Title",
+                      type: "string",
+                    },
+                    { name: "button_link", title: "Link To", type: "url" },
+                    {
+                      name: "button_pdf",
+                      title: "PDF",
+                      type: "file",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+})
